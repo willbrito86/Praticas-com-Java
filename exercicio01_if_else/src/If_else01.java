07132fd57a7b6bc1cb3e6091ab9862190259b29e
Will Brito <willsbrito@gmail.com>
2024-02-25 15:32:03 -0300

    Update If_else01.java
    atualização do código

--- exercicio01_if_else/src/If_else01.java
+++ exercicio01_if_else/src/If_else01.java
@@ -1,28 +1,20 @@
 import java.util.Scanner;
-public class If_else01 {
-	
-	//Fazer um programa para ler um número inteiro, 
-	// e depois dizer se este número é negativo ou não
-	
-	public static void main(String[] args) {
-		
-		int a;
-		
-		Scanner sc = new Scanner(System.in);
-		
-		System.out.println("Digite um numero inteiro: ");
-		a = sc.nextInt();
-		
-		if (a > 0) {
-			System.out.println("O número digitado é positivo!");
-		}
-		else {
-				
-			System.out.println("O número digitado é negativo!");
 
-			}
-		
+public class VerificarNumero {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-	}
+        System.out.println("Digite um nÃºmero inteiro:");
+        int numero = scanner.nextInt();
 
+        if(numero < 0) {
+            System.out.println("O nÃºmero Ã© negativo.");
+        } else if(numero == 0) {
+            System.out.println("O nÃºmero Ã© zero.");
+        } else {
+            System.out.println("O nÃºmero Ã© positivo.");
+        }
+
+        scanner.close();
+    }
 }
